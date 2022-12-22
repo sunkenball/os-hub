56e37d5d78a906cb4045a452a8efdfb20aa447fc
Sunken <114361572+sunkenball@users.noreply.github.com>
2022-12-22 16:34:42 +0000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local Id      = game.PlaceId
-local Games   = "https://raw.githubusercontent.com/sunkenball/os-hub/main/games/"
+local Games   = "https://os-hub.us/games/"
 local ForEach = function(Table, Callback)
     assert(type(Table) == "table", "p1 isn't a table")
     assert(type(Callback) == "function", "p2 isn't a function")
@@ -18,6 +18,7 @@ local Load    = function(Url, Supported)
 end
 local Urls    = {
     ["./games/slashing-simulator-2.lua"] = {10549069562},
-    ["./games/snow-shovelling-simulator.lua"] = {1252559098}
+    ["./games/snow-shovelling-simulator.lua"] = {1252559098},
+    ["./games/button-simulator-x.lua"] = {10970958609}
 }
 ForEach(Urls, Load)
